14278074a566073cb36d3175cee29e5fae3b5f16
dianag19 <diana.gomez.caicedo@correounivalle.edu.co>
2020-06-27 18:02:07 -0500

    update conection

--- Backend/server/config/config.js
+++ Backend/server/config/config.js
@@ -6,22 +6,22 @@ var pg = require('pg');
 //or native libpq bindings
 //var pg = require('pg').native
 
-var conString = "postgres://enptunho:OZpMB-cnrs9s0DpTj3LXXM5CIfJ1kLCw@ruby.db.elephantsql.com:5432/enptunho" //Can be found in the Details page
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  
-});
+// var conString = "postgres://enptunho:OZpMB-cnrs9s0DpTj3LXXM5CIfJ1kLCw@ruby.db.elephantsql.com:5432/enptunho" //Can be found in the Details page
+// var client = new pg.Client(conString);
+// client.connect(function(err) {
+// if(err) {
+//   return console.error('could not connect to postgres', err);
+// }
+
+// });
 //==========DB connection =====//
-// module.exports.connectionData = process.env.connectionData || {
-//     user: 'enptunho',
-//     host: 'ruby.db.elephantsql.com',
-//     database: 'enptunho',
-//     password: 'OZpMB-cnrs9s0DpTj3LXXM5CIfJ1kLCw',
-//     port: 5432,
-// };
+module.exports.connectionData = process.env.connectionData || {
+    user: 'enptunho',
+    host: 'ruby.db.elephantsql.com',
+    database: 'enptunho',
+    password: 'OZpMB-cnrs9s0DpTj3LXXM5CIfJ1kLCw',
+    port: 5432,
+};
 //==========================//
 
 //=========SEED FOR HASH FUNCTIONS ====//
